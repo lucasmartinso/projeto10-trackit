e1ffad530a2c421f386d02ca7d0effef92a9b0e2
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-05-24 19:56:24 -0300

    Criou

---
+++ src/components/App.js
@@ -0,0 +1,8 @@
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+
+export default function App() {
+    return(
+        <h1>Bom diaaaaaa</h1>
+    )
+}
